b4a98c86a8cbe3b4db7d97bdbb4fe0437632f391
ericnorris <erictnorris@gmail.com>
2014-10-17 00:04:22 -0400

    Filling out S3Request class.

--- S3.php
+++ S3.php
@@ -15,14 +15,20 @@ class S3 {
         $this->secretKey = $secretKey;
     }
 
-    public function putObject($bucket, $uri, $file, $headers = null) {
-
+    public function putObject($bucket, $uri, $file, S3Headers $headers) {
+        // TODO
     }
 }
 
 class S3Headers {
 
-    private $headers;
+    public $headers;
+    public $amz_headers;
+
+    public function __construct() {
+        $this->headers = array();
+        $this->amz_headers = array();
+    }
 
     public function withContentLength($size) {
         $headers['Content-Length'] = (int)$size;
@@ -33,6 +39,10 @@ class S3Headers {
         $headers['Host'] = $host;
         return $this;
     }
+
+    public function getHTTPHeaders() {
+        // TODO
+    }
 }
 
 class S3Request {
@@ -41,34 +51,21 @@ class S3Request {
 
     private $s3_action;
     private $endpoint;
-    private $virtualHosting;
-    private $bucket;
     private $uri;
-    private $headers;
+    private $s3_headers;
     private $curl_opts;
 
-    public function __construct($s3_action, $endpoint, $virtualHosting) {
+    public function __construct($s3_action, $endpoint, $uri) {
         $this->s3_action = $s3_action;
-        $this->endpoint = $endpoint;
-        $this->virtualHosting = $virtualHosting;
-    }
-
-    public static function put($endpoint, $virtualHosting) {
-        return new S3Request(self::PUT, $endpoint, $virtualHosting);
-    }
-
-    public function inBucket($bucket) {
-        $this->bucket = $bucket;
-        return $this;
-    }
-
-    public function atURI($uri) {
         $this->uri = $uri;
-        return $this;
     }
 
-    public function withHeaders($headers) {
-        $this->headers = $headers;
+    public static function put($endpoint, $uri) {
+        return new S3Request(self::PUT, $endpoint, $uri);
+    }
+
+    public function withS3Headers(S3Headers $s3_headers) {
+        $this->s3_headers = $s3_headers;
         return $this;
     }
 
@@ -78,11 +75,8 @@ class S3Request {
     }
 
     public function sign($accessKey, $secretKey) {
-        $amz_headers = array_filter($this->headers, function($header) {
-            return stripos($header, 'x-amz-') === 0;
-        }, ARRAY_FILTER_USE_KEY);
-
-        $amz_headers = array_change_key_case($amz_headers);
+        $amz_headers = $this->s3_headers->amz_headers;
+        $amz_headers = array_change_key_case($amz_headers, CASE_LOWER);
         ksort($amz_headers);
 
         $canonical_amz_headers = array();
@@ -90,17 +84,13 @@ class S3Request {
             $canonical_amz_headers[] = "$header:$value";
         }
 
-        $canonical_resource =
-            $this->bucket ? ('/' . $this->bucket : '') .
-            $this->uri;
-
         $string_to_sign = implode(array(
             $this->s3_action,
             $this->headers['Content-MD5'],
             $this->headers['Content-Type'],
             $this->headers['Date'],
             implode($canonical_amz_headers, "\n"),
-            $canonical_resource
+            $this->uri
         ), "\n");
 
         $signature = base64_encode(
@@ -111,5 +101,17 @@ class S3Request {
 
     public function execute() {
         $ch = curl_init();
+        $curl_headers = $this->s3_headers->getHTTPHeaders();
+
+        curl_setopt($ch, CURLOPT_USERAGENT, 'ericnorris/amazon-s3-php');
+        curl_setopt($ch, CURLOPT_URL, "https://{$this->endpoint}/{$this->uri}");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $curl_headers);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
+        curl_setopt($ch, CURLOPT_WRITEFUNCTION, array($this, 'curlWriteFunction'));
+        curl_setopt($ch, CURLOPT_HEADERFUNCTION, array($this, 'curlHeaderFunction'));
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+        // TODO
     }
 }
